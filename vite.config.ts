d2306975bc74db39e0a9a8ac59b421e565241abc
ThangDevAlone <quangthangvtlg@gmail.com>
2025-12-03 17:15:09 +0700

    ⚡️update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  root: 'docs',
   plugins: [react()],
   build: {
-    outDir: '../docs-dist',
-    emptyOutDir: true
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'docs/index.html',
+    },
+  },
 });
-
 
